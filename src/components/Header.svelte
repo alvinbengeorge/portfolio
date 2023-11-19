188922b5f4a5d2706e60d9c7fb7f43408bbaa69b
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2023-11-19 12:01:30 +0530

    Added components

---
+++ src/components/Header.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let y;
+  let tabs = [
+    {
+      name: "Home",
+      link: "#home",
+    },
+    {
+      name: "Projects",
+      link: "#projects",
+    },
+  ];
+</script>
+
+<header
+  class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid" +
+    (y > 0
+      ? "py-4 bg-slate-950 border-violet-950"
+      : "py-6 bg-violet-900 border-transparent")}
+>
+  <h1 class="font-medium justify-left">
+    <b class="font-bold">Alvin</b>
+  </h1>
+  <div class="sm:flex items-center gap-4 hidden">
+    {#each tabs as tab, index}
+      <a href={tab.link} class='duration-200 hover:text-violet-400'><p>{tab.name}</p></a>
+    {/each}
+  </div>
+  <a class="blueShadow relative overflow-hidden px-5 py-2 rounded-full bg-white text-slate-950" href="https://github.com/alvinbengeorge" target="_blank">
+    <div class="absolute top-0 right-full w-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"></div>
+    <h4 class="relative z-9">Github</h4>
+  </a>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,35 @@
 <script>
   import "../app.css";
+  import Header from "../components/Header.svelte";
+  import Footer from "../components/Footer.svelte";
+
+  let y,
+    innerHeight = 0,
+    innerWidth = 0;
+  function goToTop() {
+    document.body.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    });
+  }
 </script>
 
-<slot />
+<div
+  class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
+>
+  <div
+    class={"fixed bottom-9 w-full duration-200 flex p-10 z-[10] " +
+      (y > 0
+        ? "opacity-full pointer-events-auto"
+        : "pointer-events-none opacity-0")}
+  >
+    <button on:click={goToTop} class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:bg-slate-800 cursor-pointer">
+      <i class="fa-solid fa-arrow-up" />
+    </button>
+  </div>
+  <Header />
+  <slot />
+  <Footer />
+</div>
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
