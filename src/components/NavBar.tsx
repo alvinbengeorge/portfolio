7cf100c3057c81c6c821150ced03c8f9aa7caf3d
alvinbengeorge <alvinallen333@gmail.com>
2023-12-28 11:07:16 +0530

    Added basic layout for nextjs portfolio

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+
+const NavBar = () => {
+    return (
+        <nav className="shadow-box p-2 w-full bg-black">
+            <div className="flex justify-between font-bold bg-transparent">
+                <div className="text-2xl text-white">
+                    <Link href="/">Alvin</Link>
+                </div>
+                <ul className="flex justify-between text-white">
+                    <li className="mx-2">
+                        <Link href="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li className="mx-2">
+                        <Link href="/about">
+                            About
+                        </Link>
+                    </li>
+                </ul>
+                
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
