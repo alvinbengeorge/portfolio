6999e0527ad3ea76de9d3a1b465dbdec08aa37e0
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2024-02-05 09:22:33 +0000

    fix: remove heavy font

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -4,5 +4,5 @@ export const font = localFont({
     src: "../../public/fonts/Raleway-Regular.ttf"
 })
 
-export const main_font = localFont({ src: "../../public/fonts/Rubik80sFade-Regular.ttf" });
+// export const main_font = localFont({ src: "../../public/fonts/Rubik80sFade-Regular.ttf" });
 export const about_heading_font = localFont({ src: "../../public/fonts/Caveat-Regular.ttf" });
